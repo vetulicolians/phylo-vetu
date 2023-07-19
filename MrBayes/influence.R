b888dd9cccaa48e661435cdd85658340c414c5eb
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-07-19 10:15:04 +0100

    Influence calculation

--- MrBayes/influence.R
+++ MrBayes/influence.R
@@ -3,7 +3,7 @@ wd <- if (basename(getwd()) == "TreeSearch") "../MrBayes/" else
 exclusions <- list.files(wd, "^.*-no-.*\\.nex$")
 base <- substr(exclusions[1], 0, regexpr("-no-", exclusions[1]) - 1)
 nTrees <- 20
-
+Distance <- TreeDist::ClusteringInfoDistance
 
 #' @importFrom ape read.nexus
 .ReadMrBayesTrees <- function(treeFile, burninFrac) {
@@ -23,8 +23,13 @@ ReadMrBayes <- function(filename, n = NULL, burninFrac = NULL) {
   treeFiles <- list.files(dirname(filename),
                           paste0(basename(filename), "\\.run\\d+\\.t"),
                           full.names = TRUE)
-  trees <- do.call(c, lapply(treeFiles, .ReadMrBayesTrees,
-                             burninFrac = burninFrac))
+  trees <- tryCatch(do.call(c, lapply(treeFiles, .ReadMrBayesTrees,
+                                      burninFrac = burninFrac)),
+                    error = function(e) NULL)
+  if (is.null(trees)) {
+    warning("Could not read trees from ", filename)
+    return(NULL)
+  }
   if (!is.null(n)) {
     trees <- trees[seq(1, length(trees), length.out = n)]
   }
@@ -32,3 +37,17 @@ ReadMrBayes <- function(filename, n = NULL, burninFrac = NULL) {
 
 baseTrees <- ReadMrBayes(paste0(wd, base, ".nex"), n = nTrees)
 
+res <- vapply(exclusions, function(file) {
+  trees <- ReadMrBayes(paste0(wd, file), n = nTrees)
+  if (is.null(trees)) {
+    c(NA_real_, NA, NA)
+  } else {
+    thinnedTrees <- KeepTip(baseTrees, TipLabels(trees[[1]]))
+    self <- Distance(baseTrees)
+    d <- Distance(baseTrees, trees)
+    c(mean(d) - mean(self), mean(d), sd(d) - sd(self))
+  }
+}, c(meanIncrease = 0, originalMean = 0, sdIncrease = 0))
+
+colnames(res) <- substr(exclusions, nchar(base) + 5, nchar(exclusions) - 4)
+write.table(res, file = "influence.txt")
